9f87d6551942c13363bdcbb14e348d410b84ea10
Charles Walrus <awarmwalrus@gmail.com>
2021-11-30 17:09:11 -0600

    Day 1 starter code.

---
+++ advent1.py
@@ -0,0 +1,15 @@
+# Paste the input code here.
+input = ""
+
+# Leave this code in for convenience and consistency.
+def debug(msg):
+    print(" > {}".format(msg))
+
+def answer(ans):
+    print("ANS: {}".format(ans))
+
+# Start coding here.
+def advent1(input):
+    return input
+
+answer(advent1(input))
