e90face685d1b88d85014d1e1ba9434d55569324
Charles Walrus <awarmwalrus@gmail.com>
2021-12-01 00:30:15 -0600

    First day's solution.

--- advent1.py
+++ advent1.py
@@ -1,6 +1,3 @@
-# Paste the input code here.
-input = ""
-
 # Leave this code in for convenience and consistency.
 def debug(msg):
     print(" > {}".format(msg))
@@ -9,7 +6,30 @@ def answer(ans):
     print("ANS: {}".format(ans))
 
 # Start coding here.
-def advent1(input):
-    return input
+def advent1_a(lines):
+    ups = 0
+    for i in range(1, len(lines)):
+        if (int(lines[i]) > int(lines[i-1])):
+            ups = ups + 1
+    return ups
 
-answer(advent1(input))
+def advent1_b(lines):
+    windowSize = 3
+    ups = 0
+    for i in range(windowSize + 1, len(lines)+1):
+        j = i - 1
+        windowA = sum(list(map(int, lines[j-windowSize:j])))
+        windowB = sum(list(map(int, lines[i-windowSize:i])))
+        if (windowB > windowA):
+            ups = ups + 1
+        debug("{} > {}?".format(windowB, windowA))
+    return ups
+
+# Boilerplate code
+with open('input_day1.txt') as f:
+    lines = f.readlines()
+
+test_lines = ["199", "200", "208", "210", "200", "207", "240", "269", "260", "263"]
+
+answer(advent1_a(lines))
+answer(advent1_b(lines))
