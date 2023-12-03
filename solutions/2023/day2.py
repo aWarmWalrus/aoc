24589000d3d1551cc783a52dc4ca9c8f8ea788a6
Charles Qian <aWarmWalrus@users.noreply.github.com>
2023-12-03 08:46:01 -0800

    day 2

--- solutions/2023/day2.py
+++ solutions/2023/day2.py
@@ -4,16 +4,60 @@ from aoc_util import *
 Personal Stats:
       --------Part 1---------   --------Part 2---------
 Day       Time    Rank  Score       Time    Rank  Score
-  1       >24h              0       >24h              0
+  2       >24h              0       >24h              0
 """
 
 day = 2
 
+def isPullValid(pull):
+    for k in pull.split(','):
+        amt, color = k.split()
+        if color == "red":
+            if int(amt) > 12:
+                return False
+        elif color == "green":
+            if int(amt) > 13:
+                return False
+        elif color == "blue":
+            if int(amt) > 14:
+                return False
+    return True
+
+    
 def solveA(lines):
-    return 1
+    validIDs = 0
+    for l in lines:
+        gameID, pulls = l.split(':')
+        if all([isPullValid(p) for p in pulls.split(';')]):
+            gID = int(gameID.split()[1])
+            validIDs += gID
+    return validIDs
+
+
+def maxColors(game):
+    reds = 0
+    greens = 0
+    blues = 0
+    for pull in game.split(';'):
+        for k in pull.split(','):
+            amt, color = k.split()
+            if color == "red" and int(amt) > reds:
+                reds = int(amt)
+            elif color == "green" and int(amt) > greens:
+                greens = int(amt)
+            elif color == "blue" and int(amt) > blues:
+                blues = int(amt)
+    return reds, greens, blues
+
 
 def solveB(lines):
-    return 1
+    sum = 0
+    for l in lines:
+        gameID, pulls = l.split(':')
+        reds, greens, blues = maxColors(pulls)
+        sum += reds * greens * blues
+    return sum
+
 
 if __name__ == "__main__":
-    answerAndSubmit(day, solveA, solveB, 0, 0)
+    answerAndSubmit(day, solveA, solveB, 8, 2286)
---
+++ solutions/2023/day3.py
@@ -0,0 +1,21 @@
+from aoc_util import *
+
+"""
+Personal Stats:
+      --------Part 1---------   --------Part 2---------
+Day       Time    Rank  Score       Time    Rank  Score
+  3       >24h              0       >24h              0
+"""
+
+day = 3
+
+def solveA(lines):
+    return 0
+
+
+def solveB(lines):
+    return 0 
+
+
+if __name__ == "__main__":
+    answerAndSubmit(day, solveA, solveB, 1, 1)
